29678484bb47c4e5b295e260ce983abe4279181b
Elias Borges <eliasborges@unipam.edu.br>
2022-08-20 11:27:19 -0300

    fix implements swagger

--- src/main/java/com/control/hospital/config/SwaggerConfig.java
+++ src/main/java/com/control/hospital/config/SwaggerConfig.java
@@ -30,18 +30,4 @@ public class SwaggerConfig {
                 .build();
     }
 
-/*    private ApiKey apiKey() {
-        return new ApiKey("JWT", "Authorization", "header");
-    }
-
-    private SecurityContext securityContext() {
-        return SecurityContext.builder().securityReferences(defaultAuth()).build();
-    }
-
-    private List<SecurityReference> defaultAuth() {
-        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
-        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-        authorizationScopes[0] = authorizationScope;
-        return List.of(new SecurityReference("JWT", authorizationScopes));
-    }*/
 }
